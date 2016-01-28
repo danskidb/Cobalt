6fc935324c28675157ea4430c61541a9edf752ff
Danny de Bruijne <dannydb44@gmail.com>
2016-01-28 16:54:20 +0100

    Reset project

---
+++ app/src/main/java/danski/cobalt/MainActivity.java
@@ -0,0 +1,13 @@
+package danski.cobalt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

6e1f0703830ad9c2f3532fc7dd7280aeb832707b
Danny de Bruijne <dannydb44@gmail.com>
2016-07-06 16:06:51 +0200

    Overwrite your steam name on every launch, since it can change.

--- app/src/main/java/danski/cobalt/Setup/SplashActivity.java
+++ app/src/main/java/danski/cobalt/Setup/SplashActivity.java
@@ -5,11 +5,13 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import danski.cobalt.Defines;
 import danski.cobalt.Home.HomeActivity;
 import danski.cobalt.R;
 import danski.cobalt.sql.SQLManager;
+import danski.cobalt.sql.SteamprofileRetreiver;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -25,6 +27,19 @@ public class SplashActivity extends AppCompatActivity {
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if(prefs.getBoolean("setupcomplete", false)){
+            Thread t = new Thread(){
+                @Override
+                public void run(){
+                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(Defines.CurrentContext);
+                    try{
+                        new SteamprofileRetreiver().retreivePlayerDetails(prefs.getLong("steamid64", 0));
+                    } catch (Exception e){
+                        Log.w("SplashActivity", "Uh oh... Couldn't retreive the updated profile. No worries though.");
+                    }
+                }
+            };
+            t.start();
+
             Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
             finish();

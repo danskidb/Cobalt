92cce69d9f5fb804f87dd0dc3f7b95fb6bf6cb0f
Danny de Bruijne <dannydb44@gmail.com>
2016-07-10 17:42:14 +0200

    A beginning for the 'me' screen

--- app/src/main/java/danski/cobalt/Home/home_me.java
+++ app/src/main/java/danski/cobalt/Home/home_me.java
@@ -1,16 +1,28 @@
 package danski.cobalt.Home;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import danski.cobalt.R;
+import danski.cobalt.sql.Player;
+import danski.cobalt.sql.SQLManager;
 
 public class home_me extends Fragment {
+
+    ImageView profilePicture;
+    TextView profileName;
 
     public home_me() {
         // Required empty public constructor
@@ -31,7 +43,19 @@ public class home_me extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_me, container, false);
+        View v = inflater.inflate(R.layout.fragment_home_me, container, false);
+
+        profilePicture = (ImageView) v.findViewById(R.id.fragment_me_profilepicture);
+        profileName = (TextView) v.findViewById(R.id.fragment_me_profilename);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        if(SQLManager.instance == null) new SQLManager(getContext());
+        Player p = SQLManager.instance.getPlayer(prefs.getLong("steamid64", 0));
+
+        profileName.setText(p.Name);
+        Picasso.with(getContext()).load(p.URL_avatarfull).into(profilePicture);
+
+        return v;
     }
     
 
